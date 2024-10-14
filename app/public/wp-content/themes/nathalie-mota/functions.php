ba19659f5271fb359a558ee110608c9ce85dfedc
Matheo TRIKI <matheo.triki@gmail.com>
2024-10-14 21:13:05 +0200

    Ajout de la modale de contact et du JS pour gérer son affichage

--- app/public/wp-content/themes/nathalie-mota/functions.php
+++ app/public/wp-content/themes/nathalie-mota/functions.php
@@ -19,7 +19,8 @@ define( 'CHILD_THEME_NATHALIE_MOTA_VERSION', '1.0.0' );
 function child_enqueue_styles() {
 
 	wp_enqueue_style( 'nathalie-mota-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_NATHALIE_MOTA_VERSION, 'all' );
+	wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', array(), '1.0', true);
 
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_action( 'wp_enqueue_script', 'child_enqueue_styles', 15 );
