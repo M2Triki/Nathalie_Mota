8a6c11355627e739a807a82c901532bde265eb5d
Matheo TRIKI <matheo.triki@gmail.com>
2024-10-14 21:10:15 +0200

    Ajout fichiers header, footer et index php

---
+++ app/public/wp-content/themes/nathalie-mota/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="footer-content">
+        <p>&copy; 2024 Mon Site | Tous droits réservés</p>
+        <a href="#" id="open-contact-modal">Contactez-nous</a>
+    </div>
+    <?php wp_footer(); ?>
+</footer>
+</body>
+</html>
---
+++ app/public/wp-content/themes/nathalie-mota/index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+<main>
+  <h1>Bienvenue sur mon site personnalisé</h1>
+</main>
+<?php get_footer(); ?>
