a2a46b3b2a6e55157acf272e6a52329e530d76aa
Matheo TRIKI <matheo.triki@gmail.com>
2025-01-04 18:16:11 +0100

    Ajout title et description header

--- app/public/wp-content/themes/nathalie-mota/header.php
+++ app/public/wp-content/themes/nathalie-mota/header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title(); ?></title>
+    <title>Nathalie Denis</title>
+    <meta name="description" content="Site pour Nathalie Denis, fait par Matheo TRIKI">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
